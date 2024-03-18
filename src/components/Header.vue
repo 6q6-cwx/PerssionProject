f3bc63850cb3a951b26f8fbfd319ecaae3e00a20
jiafeimao <1245108538@qq.com>
2024-03-19 00:50:18 +0800

    修改header样式

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,53 +1,55 @@
 <template>
-  <!-- <div>
-    <div>
-      <el-row>
-        <el-button>默认按钮</el-button>
-        <el-button type="primary">主要按钮</el-button>
-        <el-button type="success">成功按钮</el-button>
-        <el-button type="info">信息按钮</el-button>
-        <el-button type="warning">警告按钮</el-button>
-        <el-button type="danger">危险按钮</el-button>
-      </el-row>
-    </div>
-  </div> -->
   <el-header>
     <div class="header">
-      <div>
-        <img src="../assets/img/header-logo.png" width="200"/>
-        <span>权限管理系统</span>
+      <div class="left">
+        <img
+          src="../assets/img/header-logo.png"
+          width="100"
+          style="margin-right: 15px"
+        />
+        <span class="autotitle">权限管理系统</span>
       </div>
-      <div>
+      <div class="right">
         <div></div>
+        <div style="text-align: center;">姓名</div>
+        <el-button size="mini">退出</el-button>
       </div>
     </div>
-    
   </el-header>
 </template>
 
 <script>
 export default {
-  name: 'PermissionprojectHeader',
+  name: "PermissionprojectHeader",
 
   data() {
-    return {
-      
-    };
+    return {};
   },
 
-  mounted() {
-    
-  },
+  mounted() {},
 
-  methods: {
-    
-  },
+  methods: {},
 };
 </script>
 <style lang="less" scoped>
-.header{
+.header {
+  height: inherit;
   display: flex;
   justify-content: space-between;
+  align-items: center;
+  .left {
+    .autotitle {
+      // color: white;
+    }
+  }
 
+  .right {
+    display: flex;
+    gap: 5px;
+
+  }
+   .right > div:nth-child(2) {
+    line-height: 27.6px;
+  }
 }
 </style>
