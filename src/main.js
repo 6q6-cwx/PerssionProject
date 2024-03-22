40140435a7bbffeb092c202378e2a4f60932e600
Your Name <1245108538@qq.com>
2024-03-22 19:02:38 +0800

    chang-code

--- src/main.js
+++ src/main.js
@@ -6,22 +6,7 @@ import '../src/assets/css/index.css'
 import './moke/globaldata'
 import './server/api'
 
-
-// httpServ.get('user/baseInfo').then(res=>{
-//   console.log(res)
-// })
-
-
-
-
-
-// import { } from '@/assets/js/utils'
-// import { getMenu } from '../src/server/api'
-
 console.log(process.env.NODE_ENV,'env')
-// await getMenu().then(res=>{
-//   console.log(res,'res')
-// })
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
--- src/server/api.js
+++ src/server/api.js
@@ -1,10 +1,20 @@
 import axios  from "axios";
 
 const httpServe=axios.create({
-    baseURL:'http://localhost:1024',
+    baseURL:'http://localhost:3000',
     timeout: 10000,
 })
 
+httpServe.getMoke=(url)=>{
+    return new Promise((resolve,reject)=>{
+      axios.get(url).then((res)=>{
+        console.log(res,'res')
+        resolve(res)
+      }).catch(()=>{
+        reject(new Error('Faile request'))
+      })
+    })
+}
 
 httpServe.interceptors.request.use((config) => {
     // 在这里可以对请求进行预处理
@@ -12,37 +22,85 @@ httpServe.interceptors.request.use((config) => {
     // config.headers.Authorization = 'Bearer ' + token;
     return config;
   }, (error) => {
-    // 请求错误处理
     return Promise.reject(error);
   });
 
   httpServe.interceptors.response.use(
     (response) => {
-      // 在这里可以对响应数据进行处理
-      // 例如统一处理返回的数据格式
+      const { data } = response
+      console.log(response,'response')
       if (response.data.code !== 0) {
         console.error('请求失败：', response.data.message);
       }
-      return response;
+      return {
+        data
+      };
     },
     (error) => {
-      // 响应错误处理
       return Promise.reject(error);
     }
   );
 
-  httpServe.postMethod = function(url, data) {
-    return this.post(url, data);
+  httpServe.getMethod = ({url,data,params},config)=> {
+    const param={
+      url: url,
+      method: 'post',
+      params: params,
+      data: data,
+      ...config
+    }
+    if(process.env.NODE_ENV === 'development'){
+      httpServe.getMoke(url,data)
+      return
+    }
+    return new Promise((resolve,reject)=>{
+      httpServe.get(param).then(res=>{
+        resolve(res)
+      }).catch((e)=>{
+        reject(e)
+      })
+    })
   };
 
+  httpServe.postMethod = ({url,data},config)=> {
+    const param={
+      url: url,
+      data: data,
+      ...config
+    }
+    return new Promise((resolve,reject)=>{
+      httpServe.post(param.url,param).then(res=>{
+        resolve(res)
+      }).catch((e)=>{
+        reject(e)
+      })
+    })
+  };
+  //请求二进制流文件 
+  httpServe.getStream = ({url,data,params},config)=> {
+    const param={
+      url: url,
+      method: 'post',
+      // Accept 告诉服务器，客户端能够接收哪些类型的响应数据（告诉服务器期望数据类型）
+      // Content-Type 告诉服务器或客户端——发送的数据的类型(请求头，响应头中能看到请求响应的数据类型)
+      // responseType  期望服务器如何返回响应数据 (期望如何接收响应数据)
+      headers: {'Accept': 'application/octet-stream','responseType':'blob'},//设置不固定 
+      params: params,
+      data: data,
+      ...config
+    }
+    return new Promise((resolve,reject)=>{
+      httpServe.post(param).then(res=>{
+        let blob = res.data;
+        const file = window.URL.createObjectURL(blob);
+        resolve(file);
+        window.URL.revokeObjectURL(blob);
+      }).catch((e)=>{
+        reject(e)
+      })
+    })
+  };
+
+  httpServe.postMethod({url:'/'})
+
   export default httpServe
-
-
-
-//   httpServe.postMethod('/api/data', { key: 'value' })
-//   .then(response => {
-//     console.log(response.data);
-//   })
-//   .catch(error => {
-//     console.error(error);
-//   });
